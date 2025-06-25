2d07efecb3407c904af7efa29176ac1578ea7c55
Edmond Huang <vincent20100517@hotmail.com>
2025-06-24 20:53:53 -0700

    Add typing for states

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,9 @@
-export const LIST_OF_NAV_TABS: { id: string; label: string }[] = [
+type NavTabsType = { id: string; label: string };
+const LIST_OF_NAV_TABS: NavTabsType[] = [
   { id: "upcomingReleases", label: "Upcoming" },
   { id: "pastReleases", label: "Past Releases" },
 ];
 
-export const DEFAULT_TAB = LIST_OF_NAV_TABS[0].id;
+const DEFAULT_TAB = LIST_OF_NAV_TABS[0].id;
+
+export { LIST_OF_NAV_TABS, DEFAULT_TAB };
